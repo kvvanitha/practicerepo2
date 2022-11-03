6e9d8faa36e96ff6fd5c9f3201db051eb103a193
kvvanitha <kvvanitha81@gmail.com>
2022-11-03 15:06:18 +0530

    added admin.h

---
+++ admin.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class Admin
+{
+
+private:
+    string AdminuserName = "Admin@123";
+    string AdminPassword = "1234567";
+public:
+    //this function is used to display the admin menu
+    void Adminmenu();
+
+    //this function is used for the admin login
+    void login();
+    string getvalue(char* filename, string user);
+    void setvalue(char* filename, char* tempfile, string user, string updatevalue);
+    
+    void AddProduct();
+
+    void removepro(char* filename, char* tempfile, string proid);
+    void RemoveProduct();
+    void Discount();
+    void modify();
+
+};
